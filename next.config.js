648f4c45c009d7ae47839553573f17b0a67d0174
Piero A. Guzman Mora <pierogm@ensign.edu>
2025-02-24 22:24:21 -0700

    Updated portfolio build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
   pageExtensions: ['jsx', 'js'],
   webpack: (config) => {
     config.resolve.modules.push(__dirname);
