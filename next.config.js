c3f1b4c03bdadff9eead5c841f8b134864513350
Piero A. Guzman Mora <pierogm@ensign.edu>
2025-02-24 22:32:24 -0700

    Comparto

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
+  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "/portfolio/" : "",
   pageExtensions: ['jsx', 'js'],
   webpack: (config) => {
     config.resolve.modules.push(__dirname);
